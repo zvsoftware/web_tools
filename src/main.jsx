8aa768229505a504b4cd2f0b455d6c92db34813f
T.R.M. Batt <73239367+timbatt@users.noreply.github.com>
2025-07-29 19:57:50 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </main>
     <footer className="footer py-12 text-gray-400">
-      <p className="mb-4">View on <a href="https://github.com/timbatt/image-optimizer" target="_blank">Github</a></p>
+      <p className="mb-4"><a href="https://apps.zoarvalleysoftware.com/" target="_blank">View on Github</a></p>
       <p>DISCLAIMER: We are not responsible for any potential data loss or data corruption that may result from the use of this tool.</p>
     </footer>
   </StrictMode>
